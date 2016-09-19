d10f4f36f63ed745145e056e81957e32bde4971f
Liu Ming <liuming@Richs-MacBook-Pro.local>
2016-09-19 15:21:53 +1200

    move to webpack

---
+++ src/components/NavHeader.vue
@@ -0,0 +1,28 @@
+<script >
+	import { aside } from 'vue-strap'
+	export default {
+		
+		props:['showLeft'],
+		// {
+			// data:{
+			// 	type:Object,
+			// 	required: true
+			// }
+		// 	showLeft:
+		// }
+		components:{
+			aside
+		},
+
+	}
+</script>
+
+<template>
+	<aside :show.sync="showLeft" placement="left" header="Title" width="350">
+		<ul>
+			<li> login </li>
+			<li> messages </li>
+			<li> friends </li>
+		</ul>
+	</aside>
+</template>
--- src/views/FeedsView.vue
+++ src/views/FeedsView.vue
@@ -1,5 +1,6 @@
 <script >
 	import FeedCom from '../components/FeedCom.vue'
+	import NavHeader from '../components/NavHeader.vue'
 	export default{
 		data(){
 			return{
@@ -9,40 +10,52 @@
 			}
 		},
 		components: {
-			FeedCom
+			FeedCom,
+			NavHeader
 		},
 		route:{
 			data(transition){
 				this.getTopics()
 
-				//
-				// $(window).on('scroll',()=>{
-				// 	this.getScrollData();
-				// });
+				window.addEventListener('scroll',this.getScrollData);
 			}
 		},
 		methods:{
 			getTopics(){
-				let url = 'https://mojo-backend-mingliu.c9users.io/v2/feeds/entries.json?page=1'
+				let url = 'https://mojo-backend-mingliu.c9users.io/v2/feeds/entries.json?page='+this.page
 				console.log(url)
 				// var requestHeaders = {'Content-Type':'application/json'}
 				this.$http.get(url)
 					.then(response=>{
 						this.feeds = this.feeds.concat(response.data);
-						// this.feeds = response.data
+						this.scroll = true;
 					})
 			},
-			// getScrollData(){
-			// 	if(this.scroll){
-			// 		let totalheight = parseFloat($(window).height()) + parseFloat($(window).scrollTop());
-			// 		if($(document).height() <= totalheight +200){
-			// 			this.scroll = false;
-			// 			this.page++;
-			// 			this.getTopics();
-			// 		}
-			// 	}
-			// }
+			getScrollData(){
+				if(this.scroll){
+					let totalheight = parseFloat(window.innerHeight + parseFloat(window.scrollY));
+
+					var body = document.body,
+    				html = document.documentElement;
+
+					var height = Math.max( body.scrollHeight, body.offsetHeight, 
+                       html.clientHeight, html.scrollHeight, html.offsetHeight );
+					if(height <= totalheight +200){
+						this.scroll = false;
+						this.page++;
+						this.getTopics();
+						console.log('get scroll data')
+					}
+				}
+			}
+		},
+		beforeDestroy:function(){
+			window.removeEventListener('scroll',this.getScrollData)
+		},
+		ready:function(){
+			this.showLeft = true;
 		}
+
 	}
 
 </script>
@@ -58,9 +71,13 @@
 </style>
 
 <template>
-	<section class="feed-list">
-		<feed-com v-for='feed in feeds' :data='feed'>
+	<div>
+		<nav-header :show-left="showLeft"></nav-header>
+		<section class="feed-list">
+			<feed-com v-for='feed in feeds' :data='feed'>
 			
-		</feed-com>	
-	</section>
+			</feed-com>	
+		</section>
+	</div>
+	
 </template>
